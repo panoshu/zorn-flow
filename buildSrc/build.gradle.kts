163fdd1c7d2cce57bfec9177e418f0b9a2858051
panoshu <5207725+panoshu@users.noreply.github.com>
2025-08-30 19:59:59 +0800

    kotlin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,14 +4,22 @@ plugins {
 
 repositories {
   mavenLocal()
-  maven("https://maven.aliyun.com/repository/public")
-  maven("https://maven.aliyun.com/repository/gradle-plugin")
   mavenCentral()
   gradlePluginPortal()
+  maven("https://maven.aliyun.com/repository/public")
+  maven("https://maven.aliyun.com/repository/gradle-plugin")
 }
+
+val kotlinVersion = libs.versions.kotlin.get()
 
 dependencies {
   implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
+
+  implementation(kotlin("gradle-plugin", version = kotlinVersion))
+  implementation(kotlin("scripting-common", version = kotlinVersion))
+  implementation(kotlin("scripting-jvm", version = kotlinVersion))
+  implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:$kotlinVersion")
+  implementation("org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable:$kotlinVersion")
 
   implementation(libs.spring.boot.gradle.plugin)
   implementation(libs.spring.dependency.management)
